557652816671a79e8ba7a03629f466663eaa7b73
Dmytro <Aldos2012@ukr.net>
2020-10-19 18:05:36 +0300

    final

---
+++ src/components/Details/index.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
+import YouTube from "react-youtube";
+import useLaunches from "../useLaunches/useLaunches";
+import './details.css';
+import Main from "../Main";
+
+const Details = (props) => {
+
+    const [launch,setLaunch] = useState(null);
+    const {getLaunch} = useLaunches();
+
+    useEffect(() => {
+        setLaunch(getLaunch(props.match.params.id));
+    }, [getLaunch])
+
+    const history = useHistory();
+
+    if (!launch) return <div>One moment...</div>
+
+    return(
+    <>
+        <Main name={launch.name} />
+        <main className="details">
+            <div className="container">
+                <div className="details-row">
+                    <div className="details-image">
+                        <img src={launch.links.patch.small} alt="launch.name"/>
+                    </div>
+                    <div className="details-content">
+                        <p className="details-description">{launch.details}</p>
+                    </div>
+                </div>
+                <div>
+                <YouTube className='details-youtube' videoId={launch.links.youtube_id} />
+                </div>
+            </div>
+            <a onClick={history.goBack} className="button button-back">go back</a>
+        </main>
+    </>
+
+)};
+export default Details;
